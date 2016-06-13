51e4f2125ae01f860dba39091c77c3ef123a289d
JasonQuan <jason422849464@gmail.com>
2016-06-13 19:27:04 +0800

    add split out export extension

--- quick-platform/src/main/java/com/quick/ext/primefaces/base/exporter/ExtExporterFactory.java
+++
@@ -1,9 +0,0 @@
-package com.quick.ext.primefaces.base.exporter;
-
-import org.primefaces.component.export.Exporter;
-
-public interface ExtExporterFactory {
-
-    Exporter getExporterForType(String type);
-
-}
